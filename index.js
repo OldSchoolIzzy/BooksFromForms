53321536fc2d13ce3f3f53f1fc5ad87bf29e025c
OldSchoolIzzy <herreraisrael79.ih@gmail.com>
2022-11-02 09:23:31 -0500

    bootstrap tweaks
    made it prettier

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const path = require("path");
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'pug')
-app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
 app.use(bodyParser.json())
 app.use(express.json())
 app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")))
